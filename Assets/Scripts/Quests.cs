90db45b640462281e0f6564bd34a3069a1b8bfcd
Phoder1 <alon.talmi@gmail.com>
2020-12-24 23:24:36 +0200

    Moves objects and Created Quests scripts

---
+++ Assets/Scripts/Quests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Quests
+{
+    
+    public abstract class QuestAbstSO : ScriptableObject
+    {
+
+
+    }
+
+    [CreateAssetMenu(fileName = "New Quest", menuName = "Quest")]
+    public class UnlockDoorQuest : QuestAbstSO
+    {
+
+    }
+}
