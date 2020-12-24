64dedf25a46362dbee5c4655bed91d6b5c6192c6
Rei <reisegev@yahoo.com>
2020-12-25 00:11:43 +0200

    Inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Inventory _instance;
+
+    ObjectAbst[] inventory = new ObjectAbst[10];
+
+
+    public ObjectAbst[] GetInventory { get { return inventory; } }
+    private void Awake()
     {
-        
+        _instance = this;
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            inventory[i] = null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void AddToInventory(ObjectAbst item)
     {
-        
+        if (item == null)
+            return;
+
+
+        for (int i = 0; i < inventory.Length; i++)
+        {
+
+            if (inventory[i] == null)
+            {
+                inventory[i] = item;
+
+                break;
+            }
+        }
+    }
+    public void RemoveFromInventory(ObjectAbst item)
+    {
+        if (item == null)
+            return;
+
+
+        for (int i = 0; i < inventory.Length; i++)
+        {
+
+            if (inventory[i].name == item.name)
+            {
+                inventory[i] = null;
+
+                break;
+            }
+        }
+    }
+
+    public void CheckIfObjectIsSelectable(int x)
+    {
+
+        if (inventory[x].isSelectAble)
+        {
+            inventory[x].UseObject();
+        }
+
     }
 }
--- Assets/Scripts/ObjectAbst.cs
+++ Assets/Scripts/ObjectAbst.cs
@@ -1,34 +1,44 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Object", menuName = "Object/Generic")]
 public class ObjectAbst : ScriptableObject
 {
 
     public string Name;
+    public bool isSelectAble;
     public bool canInteract;
     public bool isPickUp;
+    public string emptySentence;
     public void ToPickUp()
     {
+        // add to inventory
+
         if (!isPickUp)
             return;
 
 
-        // add to inventory
+        Inventory._instance.AddToInventory(this);
     }
 
 
-    public void WorldInteraction(ObjectAbst objectAabst)
+    public virtual void WorldInteraction(ObjectAbst objectAabst)
     {
         if (!canInteract)
+        {
             return;
+        }
+            
 
         if (objectAabst == null)
         {
+            //noraml interaction
+            Debug.Log(emptySentence);
             ToPickUp();
         }
         else
         {
+            //unlockQuest
+           
             // QuestManager._instance.CheckInteraction(this , objectAabst);
         }
 
@@ -45,4 +55,53 @@ public class ObjectNote : ObjectAbst
     [SerializeField] string ReadContent;
 
     public override void UseObject() { Debug.Log(ReadContent); }
+}
+public class ElectricitySwitch : ObjectAbst
+{
+    [SerializeField] string ReadContent = "You Turned The Light To";
+   public bool isLightOn = false;
+    public bool GetSetLightSwitchState {
+        get { return isLightOn; }
+        set {
+            isLightOn = value;
+
+            if (!isLightOn)
+            {
+                // QuestManager._instance.UnFinishQuest(this)
+            }
+            else
+            {
+                // QuestManager._instance.FinishQuest(this)
+            }
+
+        }
+    
+    }
+
+
+    public override void WorldInteraction(ObjectAbst objectAabst)
+    {
+        if (!canInteract)
+          return;
+        
+        if (objectAabst == null)
+        {
+            GetSetLightSwitchState = !GetSetLightSwitchState;
+            UseObject();
+        }
+    }
+
+    public override void UseObject() { Debug.Log(ReadContent +" "+ isLightOn); }
+}
+public class ElectricDoor : ObjectAbst
+{
+    [SerializeField] string ReadContent = "The Door Is Locked!";
+   
+    public override void UseObject() { Debug.Log(ReadContent); }
+}
+public class ACS: ObjectAbst //access control system - קודן
+{
+    [SerializeField] string ReadContent = "You Turned The Light To";
+    
+  //  public override void UseObject() { Debug.Log(ReadContent + " " + isLightOn); }
 }
