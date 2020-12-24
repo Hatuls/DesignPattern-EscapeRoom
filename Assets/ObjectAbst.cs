7f9378233316663220ca7de81a911d5e7ed4a37d
Rei <reisegev@yahoo.com>
2020-12-24 23:20:39 +0200

    Added object

---
+++ Assets/ObjectAbst.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Object", menuName = "Object/Generic")]
+public class ObjectAbst : ScriptableObject
+{
+
+    public string Name;
+    public bool canInteract;
+    public bool isPickUp;
+    public void ToPickUp()
+    {
+        if (!isPickUp)
+            return;
+
+
+        // add to inventory
+    }
+
+
+    public void WorldInteraction(ObjectAbst objectAabst)
+    {
+        if (!canInteract)
+            return;
+
+        if (objectAabst == null)
+        {
+            ToPickUp();
+        }
+        else
+        {
+            // QuestManager._instance.CheckInteraction(this , objectAabst);
+        }
+
+
+    }
+
+    public virtual void UseObject() { }
+}
+
+
+[CreateAssetMenu(fileName = "Object Note", menuName = "Object/Note")]
+public class ObjectNote : ObjectAbst
+{
+    [SerializeField] string ReadContent;
+
+    public override void UseObject() { Debug.Log(ReadContent); }
+}
