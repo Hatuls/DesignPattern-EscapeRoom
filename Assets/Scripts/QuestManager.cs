4575544067012d3ec8e0d198a5b153037dc36b96
Phoder1 <alon.talmi@gmail.com>
2020-12-25 02:06:12 +0200

    Oh my god I feel stupid, good night

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-
+    [SerializeField]
+    QuestHandler[] quests;
     List<QuestAbstSO> unfinishedQuests = new List<QuestAbstSO>();
     List<QuestAbstSO> finishedQuests = new List<QuestAbstSO>();
+    private void Awake() {
+        foreach (QuestHandler questHandler in quests) {
+            questHandler.quest.handlingScript = questHandler;
+        }
+    }
 
     public void SwapQuestState(ObjectAbst object1, ObjectAbst object2) {
         foreach(QuestAbstSO quest in unfinishedQuests) {
@@ -17,6 +24,20 @@ public class QuestManager : MonoBehaviour
     }
     public void SwapQuestState(ObjectAbst object1) {
         SwapQuestState(object1, null);
+    }
+}
+
+[Serializable]
+public class QuestHandler
+{
+    public QuestAbstSO quest;
+    [Header("Swaps the objects state on quest finish")]
+    public GameObject[] objectsToSwap;
+
+    public void SwapObjects() {
+        foreach (GameObject obj in objectsToSwap) {
+            obj.SetActive(!obj.activeSelf);
+        }
     }
 }
 
