9c09474bba80959eea57d180e41415ea86c41a70
Phoder1 <alon.talmi@gmail.com>
2021-02-04 16:24:02 +0200

    Leantween

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour
+{
+    
+    [SerializeField] private Transform[] posAnchorsTransforms;
+    [SerializeField] private int currentPosIndex;
+    [SerializeField] private float transitionSpeed;
+    int lastFramePosIndex;
+    private void Update() {
+        if(currentPosIndex != lastFramePosIndex) {
+            LeanTween.cancel(gameObject);
+            lastFramePosIndex = currentPosIndex;
+            LeanTween.move(gameObject, transform.position, transitionSpeed);
+            LeanTween.rotate(gameObject, transform.position, transitionSpeed);
+        }
+    }
+
+}
