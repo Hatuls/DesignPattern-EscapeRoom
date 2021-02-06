e3409d5484d55dd30a9a1b1a9cf6354af7c1e09e
Phoder1 <alon.talmi@gmail.com>
2021-02-06 23:43:16 +0200

    Lantern

---
+++ Assets/Scripts/LanternMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternMove : MonoBehaviour
+{
+    [SerializeField] float rotationAngle;
+    [SerializeField] float rotationTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(Vector3.right * -rotationAngle);
+        LeanTween.rotateAround(gameObject, Vector3.right, 2 * rotationAngle, rotationTime).setEaseInOutCubic().setLoopPingPong();   
+    }
+}
