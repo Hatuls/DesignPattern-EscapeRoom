e4bb3998bb4f1ba056c7aaa30cce3e098a6f5d64
Phoder1 <alon.talmi@gmail.com>
2021-02-06 11:55:50 +0200

    Camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 
-[ExecuteInEditMode]
 public class CameraController : MonoBehaviour
 {
-    
-    [SerializeField] private Transform[] posAnchorsTransforms;
-    [SerializeField] private int currentPosIndex;
     [SerializeField] private float transitionSpeed;
-    int lastFramePosIndex;
-    private void Update() {
-        if(currentPosIndex != lastFramePosIndex) {
-            LeanTween.cancel(gameObject);
-            lastFramePosIndex = currentPosIndex;
-            LeanTween.move(gameObject, transform.position, transitionSpeed);
-            LeanTween.rotate(gameObject, transform.position, transitionSpeed);
-        }
+    private Transform currentTransform;
+    private static bool inTransition = false;
+    public static bool GetInTransition => inTransition;
+    public static CameraController _instance;
+    private void Awake() {
+        if (_instance == null)
+            _instance = this;
+        else if (_instance != this)
+            Destroy(gameObject);
+        currentTransform = transform;
     }
+    public void SetAnchor(Transform newTransform) {
+        if (newTransform == null)
+            return;
+        if (currentTransform != newTransform) {
+            inTransition = true;
+            LeanTween.cancel(gameObject);
+            //LeanTween.move(gameObject, posAnchorsTransforms[currentPosIndex].position, transitionSpeed);
+            //LeanTween.rotateY(gameObject, posAnchorsTransforms[currentPosIndex].rotation.eulerAngles.y, transitionSpeed).setOnComplete(() => { inTransition = false; });
+        }
 
+    }
 }
