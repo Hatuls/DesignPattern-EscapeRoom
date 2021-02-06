20366a6428c6f6d4e0456cf2b00cf03d41027f8d
Rei <reisegev@yahoo.com>
2021-02-06 19:53:03 +0200

    scene

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,10 +7,15 @@ public class UIManager : MonoBehaviour
     static UIManager _instance;
     [SerializeField] GameObject[] inventoryPF;
     Inventory inventoryScript;
-    [SerializeField] Button LeftArrows, RightArrows;
+    CameraController cmra;
+    View currentView;
     [SerializeField] Sprite defaultSprite;
+    [SerializeField] Button LeftArrow, RightArrow , ForwardArrow , BackwardArrow;
     ButtonSlot[] inventoryButtonsSlots;
     ObjectSO[] InventroyArray;
+    public void SetActiveView(View view) { currentView = view;
+        SetVisabilityViewAllButtons(true);
+    }
     public static UIManager GetInstance => _instance;
     private void Awake() {
         _instance = this;
@@ -19,9 +24,9 @@ public class UIManager : MonoBehaviour
     private void Init() {
         inventoryScript = Inventory.GetInstance;
         InventroyArray = inventoryScript.GetInventory;
+        cmra = CameraController._instance;
 
-
-        if (inventoryPF == null || inventoryPF.Length == 0)
+        if (inventoryPF == null || inventoryPF.Length == 0|| cmra == null)
             return;
 
         if (!AssignComponenets())
@@ -42,6 +47,14 @@ public class UIManager : MonoBehaviour
             inventoryButtonsSlots[i].Init();
         return true;
     }
+
+
+
+
+
+
+
+    #region Ui Inventory
     public void UpdateUIInventory() {
 
         AssignImagesToButtons();
@@ -74,9 +87,60 @@ public class UIManager : MonoBehaviour
 
         return item.inventoryInteraction != InventoryInteraction.ActivateQuest;
 
+    }
+    #endregion
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #region View UI
+    
+    public void LookLeft() {
+        cmra.SetView(currentView.LeftView);
+    }
+    public void LookForward() {
+        cmra.SetView(currentView.ForwardView);
+    }
+    public void LookBackward() {
+
+        cmra.SetView(currentView.BackwardsView);
+    }
+    public void LookRight() {
+        cmra.SetView(currentView.RightView);
     }
 
+    void HideEveryThingExcept() { 
     
+    
+    }
+    void SetVisabilityViewAllButtons(bool ToActivate) {
 
+
+        if (ForwardArrow.gameObject.activeSelf != ToActivate)
+        ForwardArrow.gameObject.SetActive(ToActivate);
+
+        if (BackwardArrow.gameObject.activeSelf != ToActivate)
+            BackwardArrow.gameObject.SetActive(ToActivate);
+
+        if (RightArrow.gameObject.activeSelf != ToActivate)
+            RightArrow.gameObject.SetActive(ToActivate);
+
+        if (LeftArrow.gameObject.activeSelf != ToActivate)
+            LeftArrow.gameObject.SetActive(ToActivate);
+    }
+    #endregion
 }
